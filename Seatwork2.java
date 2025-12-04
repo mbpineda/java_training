febc9f74e04db3250ef687ab6337cfe794ccefc1
Myra B. Pineda <mbpineda@headoffice.corp.bpi.com.ph>
2025-12-04 16:44:46 +0800

    m2 activity 2

---
+++ Seatwork2.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package Seatwork2;
+
+/**
+ * 
+ */
+
+
+
+public class Seatwork2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+          Whiteboard wb = new Whiteboard();
+          int sumResult = wb.AddNumbers(5,3);
+          System.out.println("the total is: " + String.valueOf(sumResult));     
+
+	}
+
+}
---
+++ Whiteboard.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package Seatwork2;
+
+/**
+ * 
+ */
+public class Whiteboard {
+
+	int AddNumbers(int addend1, int addend2) {
+		int sum = addend1 + addend2;
+		return sum;
+	}
+}
